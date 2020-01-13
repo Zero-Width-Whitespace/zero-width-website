63ba68fde20e410c59db088e12a83fecaf69a153
Tobias Putman <putm1500@student.nhl.nl>
2020-01-13 13:40:47 +0100

    async test api call
    API test call to pokeapi

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 function isNotEmpty(str){
-        return (str || str.length !== 0)
+        return (str || str.length !== 0);
 }
 
-function encode() {
-        var toEncode = document.getElementById("plain-text").value
+async function encode() {
+        var toEncode = await document.getElementById("plain-text").value;
         if (isNotEmpty(toEncode)) {
-            console.log("encoding: " + toEncode)
-            //create new text area for the result
+            const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+            if (response.ok){
+                response.json().then(data => {
+                    console.log(data);
+                });
+            }
         }
 }
 
-function decode() {
-        var toDecode = document.getElementById("cypher-text").value
+async function decode() {
+        var toDecode = await document.getElementById("cypher-text").value;
         if (isNotEmpty(toDecode)) {
-            console.log("decoding: " + toDecode)
+            console.log("decoding: " + toDecode);
             //create new text area for the result
         }
 }
