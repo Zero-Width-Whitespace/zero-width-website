a4e47a90e0d27a0f1feddfc103a700cf47bb0cda
MiauwMaster <putm1500@student.nhl.nl>
2020-01-21 11:13:50 +0100

    implemented API

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
 function isNotEmpty(str){
-        return (str || str.length !== 0);
+    return (str || str.length !== 0);
 }
+
+const api_headers = new Headers({
+    'Content-Type': 'text/plain'
+});
 
 async function encode() {
     var toEncode = await document.getElementById("plain-text").value;
     if (isNotEmpty(toEncode)) {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + toEncode);
+        const response = await fetch("https://zero-width-api-web.azurewebsites.net/Encode?stringToEncode=" + toEncode, {
+            method: 'POST',
+            headers: api_headers
+        });
         if (response.ok){
-            response.json().then(data => {
-            document.getElementById("response").textContent = data.name + " " + data.id;
+            response.text().then(data => {
+                document.getElementById("response").textContent = data; 
             });
         }
-    } 
-}
+    }
+} 
+
 
 async function decode() {
     var toDecode = await document.getElementById("cypher-text").value;
     if (isNotEmpty(toDecode)) {
-        const response = await fetch("https://pokeapi.co/api/v2/pokemon/" + toDecode);
+        const response = await fetch("https://zero-width-api-web.azurewebsites.net/Decode?stringToDecode=" + toDecode, {
+            method: 'POST',
+            headers: api_headers
+        });
         if (response.ok){
-            response.json().then(data => {
-            document.getElementById("response").textContent = data.name + " " + data.id;
+            response.text().then(data => {
+                document.getElementById("response").textContent = data; 
             });
         }
     }
@@ -31,7 +42,8 @@ function clearForm(id) {
 }
 
 function clearResults() {
-    document.getElementById("response").textContent = "--Result will appear here--";
+    document.getElementById("response").textContent = `Result will appear here. When encoding, this message will disappear.
+    This means the conversion was successful. Click 'Copy' to copy your zero-width-encoded text to clipboard`;
 }
 
 function copyContent(){
