230b0d27b782ed1b5262ca1ffe51a79a592312df
MiauwMaster <putm1500@student.nhl.nl>
2020-01-21 14:02:09 +0100

    updated to multipart/form-data

--- script.js
+++ script.js
@@ -2,38 +2,30 @@ function isNotEmpty(str){
     return (str || str.length !== 0);
 }
 
-const api_headers = new Headers({
-    'Content-Type': 'text/plain'
-});
 
 async function encode() {
-    var toEncode = await document.getElementById("plain-text").value;
-    if (isNotEmpty(toEncode)) {
-        const response = await fetch("https://zero-width-api-web.azurewebsites.net/Encode?stringToEncode=" + toEncode, {
-            method: 'POST',
-            headers: api_headers
+    const formData = await new FormData(document.getElementById("encode"))
+    const response = await fetch("https://zero-width-api-web.azurewebsites.net/Encode", {
+        method: 'POST',
+        body: formData
+    });
+    if (response.ok){
+        response.text().then(data => {
+            document.getElementById("response").textContent = data; 
         });
-        if (response.ok){
-            response.text().then(data => {
-                document.getElementById("response").textContent = data; 
-            });
-        }
     }
 } 
 
 
 async function decode() {
-    var toDecode = await document.getElementById("cypher-text").value;
-    if (isNotEmpty(toDecode)) {
-        const response = await fetch("https://zero-width-api-web.azurewebsites.net/Decode?stringToDecode=" + toDecode, {
-            method: 'POST',
-            headers: api_headers
+    const response = await fetch("https://zero-width-api-web.azurewebsites.net/Decode", {
+        method: 'POST',
+        body: await new FormData(document.getElementById("decode"))
+    });
+    if (response.ok){
+        response.text().then(data => {
+            document.getElementById("response").textContent = data; 
         });
-        if (response.ok){
-            response.text().then(data => {
-                document.getElementById("response").textContent = data; 
-            });
-        }
     }
 }
 
